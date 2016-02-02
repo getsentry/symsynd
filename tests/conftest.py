8e02573d88115f8c6686da0f7ae392dad487aec1
Armin Ronacher <armin.ronacher@active-4.com>
2016-02-02 00:30:31 -0800

    Added (currently failing) swift test

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,24 +3,9 @@ import pytest
 
 
 @pytest.fixture(scope='module')
-def here():
-    return os.path.abspath(os.path.dirname(__file__))
-
-
-@pytest.fixture(scope='module')
-def res_path(here):
+def res_path():
+    here = os.path.abspath(os.path.dirname(__file__))
     return os.path.join(here, 'res')
-
-
-@pytest.fixture(scope='module')
-def ct_dsym_base_path(res_path):
-    return os.path.join(res_path, 'Crash-Tester.app.dSYM')
-
-
-@pytest.fixture(scope='module')
-def ct_dsym_path(ct_dsym_base_path):
-    return os.path.join(ct_dsym_base_path, 'Contents',
-                        'Resources', 'DWARF', 'Crash-Tester')
 
 
 @pytest.fixture(scope='function')
--- tests/test_macho.py
+++ tests/test_macho.py
@@ -1,3 +1,4 @@
+import os
 from symsynd import mach
 
 
@@ -5,7 +6,10 @@ def test_cpu_names():
     assert mach.get_cpu_name(12, 9) == 'armv7'
 
 
-def test_uuid(ct_dsym_path):
+def test_uuid(res_path):
+    ct_dsym_path = os.path.join(
+        res_path, 'Crash-Tester.app.dSYM', 'Contents', 'Resources',
+        'DWARF', 'Crash-Tester')
     uuids = mach.get_macho_uuids(ct_dsym_path)
     assert uuids == [
         '8094558b-3641-36f7-ba80-a1aaabcf72da',

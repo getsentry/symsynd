8cac6cc8babf974ca1544b8d2c251a389493a4ac
Armin Ronacher <armin.ronacher@active-4.com>
2016-04-14 10:13:31 +0200

    This is 0.6.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.environ.setdefault('CC', 'clang')
 
 setup(
     name='symsynd',
-    version='0.5.2',
+    version='0.6.0',
     url='http://github.com/getsentry/symsynd',
     description='Helps symbolicating crash dumps.',
     license='BSD',

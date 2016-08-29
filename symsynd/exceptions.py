634832b6ed70adfc3a54f6ca5a72e6b396f60d90
Armin Ronacher <armin.ronacher@active-4.com>
2016-08-30 00:51:12 +0200

    Move exception into common module and add unicode support

---
+++ symsynd/exceptions.py
@@ -0,0 +1,14 @@
+class SymbolicationError(Exception):
+    message = None
+
+    def __init__(self, message):
+        if isinstance(message, bytes):
+            message = message.decode('utf-8', 'replace')
+        Exception.__init__(self, message)
+        self.message = message
+
+    def __str__(self):
+        return self.message.encode('utf-8')
+
+    def __unicode__(self):
+        return self.message

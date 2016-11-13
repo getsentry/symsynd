f00be9ad40c11f69068d532859644418d425cd7d
Armin Ronacher <armin.ronacher@active-4.com>
2016-11-13 13:03:10 +0100

    Added tests for demangling options

--- tests/test_demangle.py
+++ tests/test_demangle.py
@@ -10,6 +10,29 @@ def test_swift_demangle():
     assert demangle_swift_symbol(mangled) == expected
 
 
+def test_swift_demangle_options():
+    mangled = (
+        '_TTWVSC29UIApplicationLaunchOptionsKeys21_ObjectiveCBridgeable'
+        '5UIKitZFS0_36_unconditionallyBridgeFromObjectiveCfGSqwx15_'
+        'ObjectiveCType_x'
+    )
+    default_expected = (
+        u'protocol witness for static Swift._ObjectiveCBridgeable._'
+        u'unconditionallyBridgeFromObjectiveC (Swift.Optional<A._'
+        u'ObjectiveCType>) -> A in conformance __C.'
+        u'UIApplicationLaunchOptionsKey : Swift._ObjectiveCBridgeable '
+        u'in UIKit'
+    )
+    simplified_expected = (
+        u'protocol witness for static _ObjectiveCBridgeable._'
+        u'unconditionallyBridgeFromObjectiveC(A._ObjectiveCType?) -> '
+        u'A in conformance UIApplicationLaunchOptionsKey'
+    )
+
+    assert demangle_swift_symbol(mangled) == default_expected
+    assert demangle_swift_symbol(mangled, simplified=True) == simplified_expected
+
+
 def test_cpp_demangle():
     mangled = '_ZN6google8protobuf2io25CopyingInputStreamAdaptor4SkipEi'
     expected = 'google::protobuf::io::CopyingInputStreamAdaptor::Skip(int)'

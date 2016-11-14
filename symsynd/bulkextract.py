b2e50dad94d3eb54e5e47b999c3c71f5c9477650
Daniel Griesser <daniel.griesser.86@gmail.com>
2016-11-14 18:21:58 +0100

    Store sdk_identifier, Made naming more generic for future usage

--- symsynd/bulkextract.py
+++ symsynd/bulkextract.py
@@ -13,7 +13,7 @@ from symsynd.macho.util import is_macho_file
 
 
 _base_path_segment = re.compile(r'^(\d+)\.(\d+)(?:\.(\d+))? \(([a-zA-Z0-9]+)\)$')
-_watch_base_path_segment = re.compile(r'^Watch(\d+),(\d+) (\d+)\.(\d+)(?:\.(\d+))? \(([a-zA-Z0-9]+)\)$')
+_advanced_base_path_segment = re.compile(r'^(Watch\d+,\d+) (\d+)\.(\d+)(?:\.(\d+))? \(([a-zA-Z0-9]+)\)$')
 
 
 NM_SEARCHPATHS = []
@@ -60,7 +60,7 @@ def get_sdk_info_from_path(sdk_name, path):
         if piece.endswith('.zip'):
             piece = piece[:-4]
         match = _base_path_segment.match(piece)
-        watch_match = _watch_base_path_segment.match(piece)
+        advanced_match = _advanced_base_path_segment.match(piece)
         if match is not None:
             tup = match.groups()
             return {
@@ -71,19 +71,17 @@ def get_sdk_info_from_path(sdk_name, path):
                 'version_patchlevel': int(tup[2] or 0),
                 'version_build': tup[3],
             }
-        elif watch_match is not None:
-            tup = watch_match.groups()
+        elif advanced_match is not None:
+            tup = advanced_match.groups()
             return {
                 'sdk_name': sdk_name,
                 'dsym_type': 'macho',
-                'watch_hardware_major': int(tup[0]),
-                'watch_hardware_minor': int(tup[1]),
-                'version_major': int(tup[2]),
-                'version_minor': int(tup[3]),
-                'version_patchlevel': int(tup[4] or 0),
-                'version_build': tup[5],
+                'sdk_identifier': tup[0],
+                'version_major': int(tup[1]),
+                'version_minor': int(tup[2]),
+                'version_patchlevel': int(tup[3] or 0),
+                'version_build': tup[4],
             }
-
 
 
 def parse_nm_line(line):

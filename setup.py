cb6dbc5fce8d8922b941f2c32c79969e62d18d2d
Armin Ronacher <armin.ronacher@active-4.com>
2017-02-15 11:07:57 +0100

    2.2.0

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ if bdist_wheel is not None:
 
 setup(
     name='symsynd',
-    version='2.1.0',
+    version='2.2.0',
     url='http://github.com/getsentry/symsynd',
     description='Helps symbolicating crash dumps.',
     license='BSD',

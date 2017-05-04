b571d08cbfa27838604aa5d998cfa89b76f97c1c
Armin Ronacher <armin.ronacher@active-4.com>
2017-05-04 17:02:13 +0200

    Added test for vmaddr

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -1,4 +1,5 @@
 import os
+from uuid import UUID
 from symsynd.libdebug import DebugInfo, get_cpu_name, get_cpu_type_tuple
 
 
@@ -17,4 +18,25 @@ def test_uuid(res_path):
     assert uuids == [
         ('armv7', '8094558b-3641-36f7-ba80-a1aaabcf72da'),
         ('arm64', 'f502dec3-e605-36fd-9b3d-7080a7c6f4fc'),
+    ]
+
+
+def test_variants(res_path):
+    ct_dsym_path = os.path.join(
+        res_path, 'Crash-Tester.app.dSYM', 'Contents', 'Resources',
+        'DWARF', 'Crash-Tester')
+    di = DebugInfo.open_path(ct_dsym_path)
+    res = sorted([x.__dict__ for x in di.get_variants()],
+                  key=lambda x: x['cpu_name'])
+    assert res == [
+        {'vmaddr': 4294967296L,
+         'vmsize': 294912L,
+         'cpu_name': u'arm64',
+         'uuid': UUID('f502dec3-e605-36fd-9b3d-7080a7c6f4fc'),
+         'name': u'<unknown>'},
+        {'vmaddr': 16384L,
+         'vmsize': 262144L,
+         'cpu_name': u'armv7',
+         'uuid': UUID('8094558b-3641-36f7-ba80-a1aaabcf72da'),
+         'name': u'<unknown>'}
     ]

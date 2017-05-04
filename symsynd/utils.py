e7ed7154c7803814396e5ae576b5d383a53ca665
Armin Ronacher <armin.ronacher@active-4.com>
2017-05-04 10:11:53 +0200

    Killed a bunch of unused code

--- symsynd/utils.py
+++ symsynd/utils.py
@@ -2,31 +2,7 @@ import os
 import time
 import threading
 from contextlib import contextmanager
-from itertools import chain
 from symsynd._compat import int_types
-
-
-def which(prog, extra_paths=None):
-    path = os.environ['PATH'].split(os.path.pathsep)
-    if extra_paths:
-        path = chain(path, extra_paths)
-    for p in path:
-        p = os.path.join(p, prog)
-        if os.path.exists(p) and os.access(p, os.X_OK):
-            return p
-
-
-@contextmanager
-def _dummy_bar(items):
-    yield items
-
-
-def progressbar(items, prefix, enabled=True):
-    if enabled:
-        label = '%-20s' % prefix
-        import click
-        return click.progressbar(items, label=label)
-    return _dummy_bar(items)
 
 
 def parse_addr(x):
